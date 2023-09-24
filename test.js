fe347c5ba60ea8c6b1f64e712c4fe7317329bfe7
mzrdev <87870017+MZRCode@users.noreply.github.com>
2023-09-24 23:04:37 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,43 +1,46 @@
 const mzr = require('./file/index');
 
-setTimeout(() => {
-    var sonuc = mzr.calculate(10, 100);
+/*setTimeout(() => {
+    const sonuc = mzr.calculate(10, 100);
     console.log('%' + sonuc + ' Percent ✔')
 }, 1000);
 
 setTimeout(() => {
-    var sonuc = mzr.random(10, 5);
+    const sonuc = mzr.random(10, 5);
     console.log(sonuc)
 }, 1500);
 
 setTimeout(() => {
-    var sonuc = mzr.shortNumber(7403);
+    const sonuc = mzr.shortNumber(7403);
     console.log(sonuc + ' ShortNumber ✔')
 }, 2000);
 
 setTimeout(() => {
-    var sonuc = mzr.mcmotd(`play.craftrise.tc`)
+    const sonuc = mzr.mcmotd(`play.craftrise.tc`)
     console.log(sonuc + ' MC Motd ✔')
 }, 2500);
 
 setTimeout(() => {
-    var sonuc = mzr.version;
-    console.log(sonuc  + ' Version ✔')
+    mzr.slashBuilder;
 }, 3000);
 
 setTimeout(() => {
-    var sonuc = mzr.addNumberDot(12381248125);
-    console.log(sonuc)
+    const sonuc = mzr.version;
+    console.log(sonuc  + ' Version ✔')
 }, 3500);
 
 setTimeout(() => {
-    const sure = 1695495014935;
-    var sonuc = mzr.timestamp(sure)
+    const sonuc = mzr.formatNumber(12381248125);
     console.log(sonuc)
-}, 4000)
+}, 4000);*/
 
 setTimeout(() => {
-    const sure = 90300;
-    var sonuc = mzr.ms(sure, { short: true, lang: 'tr', ms: true });
-    console.log(sonuc + ' MS ✔')
-}, 4500);
+    const sonuc = mzr.timestamp(1695495014935)
+    console.log(sonuc)
+}, 500)
+
+setTimeout(() => {
+    const asama1 = mzr.ms('1ay') + mzr.ms('1h') + 300;
+    const asama2 = mzr.ms(asama1, { short: false, lang: 'tr', ms: false })
+    console.log(asama2)
+}, 1000);
