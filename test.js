1cfb93c358ffbe2487b4dca5d95a8af04ad3d28a
mzrdev <87870017+MZRCode@users.noreply.github.com>
2023-10-15 20:41:14 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,46 +1,60 @@
 const mzr = require('./file/index');
+const kontr = require('./file/kelime-oyunu');
 
-/*setTimeout(() => {
+setTimeout(() => {
     const sonuc = mzr.calculate(10, 100);
     console.log('%' + sonuc + ' Percent ✔')
-}, 1000);
+}, 500);
 
 setTimeout(() => {
     const sonuc = mzr.random(10, 5);
     console.log(sonuc)
-}, 1500);
+}, 1000);
 
 setTimeout(() => {
     const sonuc = mzr.shortNumber(7403);
     console.log(sonuc + ' ShortNumber ✔')
-}, 2000);
+}, 1500);
 
 setTimeout(() => {
     const sonuc = mzr.mcmotd(`play.craftrise.tc`)
     console.log(sonuc + ' MC Motd ✔')
-}, 2500);
+}, 2000);
 
 setTimeout(() => {
     mzr.slashBuilder;
-}, 3000);
+}, 2500);
 
-setTimeout(() => {
+/*setTimeout(() => {
     const sonuc = mzr.version;
-    console.log(sonuc  + ' Version ✔')
-}, 3500);
+    console.log(sonuc + ' Version ✔')
+}, 3000);*/
 
 setTimeout(() => {
     const sonuc = mzr.formatNumber(12381248125);
     console.log(sonuc)
-}, 4000);*/
+}, 3000);
 
 setTimeout(() => {
     const sonuc = mzr.timestamp(1695495014935)
     console.log(sonuc)
-}, 500)
+}, 3500)
 
 setTimeout(() => {
-    const asama1 = mzr.ms('1ay') + mzr.ms('1h') + 300;
-    const asama2 = mzr.ms(asama1, { short: false, lang: 'tr', ms: false })
-    console.log(asama2)
-}, 1000);
+    const asama1 = '1g';
+    const asama2 = mzr.ms(asama1);
+    const asama3 = mzr.ms(asama2, { lang: 'tr', short: true, units: ['g', 'h', 'dk', 'sn'] })
+    console.log(asama3)
+}, 4000);
+
+setTimeout(() => {
+    kontr.tdk('otobüs').then((veri) => {
+        console.log('Onay: ' + veri.onay);
+        console.log('Not: ' + veri.not);
+        console.log('Kelime: ' + veri.kelime);
+        console.log('İlk Harf: ' + veri.ilkHarf);
+        console.log('Son Harf: ' + veri.sonHarf);
+        console.log('Lisân: ' + veri.lisan);
+        console.log('Anlam: ' + veri.anlam);
+    });
+}, 4500);
