4a323e397f06086aa7b3405cee5a955f1cda8ee3
mzrdev <87870017+MZRCode@users.noreply.github.com>
2024-01-15 23:16:44 +0300

    Update index.d.ts

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,8 +1,45 @@
-interface Options {
-    lang: "tr" | "en";
+interface msOptions {
+    lang: 'tr' | 'en';
     short?: boolean;
     largest?: number;
     units?: string[];
+}
+
+interface genPasswordOptions {
+    length: number;
+    numbers?: boolean;
+    lowercase?: boolean;
+    uppercase?: boolean;
+    symbols?: boolean;
+    excludeSimilarCharacters?: boolean;
+}
+
+interface genCodeOptions {
+    length?: number;
+    range?: number;
+    numbers: boolean;
+    lowercase?: boolean;
+    uppercase?: boolean;
+    excludeSimilarCharacters?: boolean;
+    symbol: string;
+}
+
+interface lbOptions {
+    dot?: boolean;
+}
+
+interface AntiCrashOptions {
+    url?: string;
+}
+
+export declare class AntiCrash {
+    private hidden
+    private hook;
+    private checkWebhook;
+    constructor(options: AntiCrashOptions);
+    setHide: (hidden: 'console' | 'webhook') => this;
+    setShow: (show: 'console' | 'webhook') => this;
+    start: () => void;
 }
 
 declare const mzrdjs: {
@@ -12,9 +49,12 @@ declare const mzrdjs: {
     mcmotd: (ip: string) => string;
     formatNumber: (input: number) => string;
     timestamp: (time: number) => number;
-    ms: (value: string | number, options: Options) => string | number;
+    ms: (value: string | number, options: msOptions) => string | number;
     version: string;
     tdk: (kelime: string) => string;
+    leaderboard: (data: string, pageSize: number, options: lbOptions) => string;
+    generatePassword: (options: genPasswordOptions) => string;
+    generateCode: (options: genCodeOptions) => string;
 }
-  
+
 export = mzrdjs;
