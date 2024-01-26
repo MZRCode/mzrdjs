3c1e58085e3f0b240dcd72eb611a4eae9e3b1993
mzrdev <87870017+MZRCode@users.noreply.github.com>
2024-01-26 16:47:41 +0300

    Update AntiCrash.js

--- file/AntiCrash.js
+++ file/AntiCrash.js
@@ -1,74 +1,52 @@
-const { WebhookClient, EmbedBuilder, codeBlock } = require('discord.js');
-const ansi = require('ansi-colors');
-const sneaks = require('sneaks');
-const axios = require('axios');
-
 class AntiCrash {
     /**
-     * Anti Crash Kurulumu
+     * Anti Crash Setup
         * @example
         * const { AntiCrash } = require('mzrdjs')
+        * 
         * new AntiCrash({ url: "DISCORD_WEBHOOK_URL" })
-     * @param {String} Webhook URL - Discorddaki bir kanalın webhook urlsi
+        * 
+        * new AntiCrash() (Recommended)
+     * @param {String} Webhook URL - Webhook URL of a channel on Discord
      */
     constructor(options) {
+        require('discord.js'); // To indicate that discord.js is a module requirement, we define it here
+        const { error } = require('../Utils/consoleTools');
+        const ansi = require('ansi-colors');
+        const axios = require('axios');
+
         this.hiddenConsole = false;
         this.hiddenWebhook = true;
 
         if (options) {
             if (typeof options === 'string') {
                 this.url = options;
-                this.checkWebhook(this.url);
             } else if (options && options.url) {
                 this.url = options.url;
-                this.checkWebhook(this.url);
             };
         };
 
         try {
             axios.get('https://registry.npmjs.org/mzrdjs/latest').then(response => {
                 const data = response.data;
-                if (require('../package.json').version !== data.version) {
-                    sneaks.console.error(`You are using an old version. You can update the module by typing '${ansi.blueBright(`npm install mzrdjs@${data.version}`)}'`);
+
+                if (require('mzrdjs/package.json').version !== data.version) {
+                    error(`You are using an old version. You can update the module by typing '${ansi.blueBright(`npm install mzrdjs@${data.version}`)}'`);
                 };
-            }).catch(error => { });
-        } catch (err) {
-
-        }
-    }
-
-    async checkWebhook(url) {
-        if (!url) return;
-
-        try {
-            const response = await axios.head(url);
-
-            if (response.status === 200) {
-                this.hook = new WebhookClient({ url });
-            } else {
-                sneaks.console.error(`Beklenmeyen bir durum oluştu. Hata kodu: ${ansi.red(response.status)}`);
-            };
-        } catch (error) {
-            if (error.response) {
-                sneaks.console.error('Webhook URL\'si yanlış!');
-            } else if (error.request) {
-                sneaks.console.error('Webhook URL\'si yanlış!');
-            } else {
-                sneaks.console.error(`Webhook URL\'sinde bir sorun mevcut. Hata kodu: ${ansi.red(error.message)}`);
-            };
-        };
+            }).catch(() => { });
+        } catch (err) { }
     }
 
     /**
         * @example
-        * new AntiCrash({ url: "DISCORD_WEBHOOK_URL" })
-        * .setHide() // Konsol ve Webhook bildirimlerini gizler.
+        * new AntiCrash()
+        * .setHide() // Hide Console and Webhook notifications
+        * 
+        * new AntiCrash()
+        * .setHide('console') // Hide console notifications
         * 
         * new AntiCrash({ url: "DISCORD_WEBHOOK_URL" })
-        * .setHide('console') // Konsol bildirimlerini gizler.
-        * 
-        * new AntiCrash({ url: "DISCORD_WEBHOOK_URL" })
-        * .setHide('webhook') // Webhook bildirimlerini gizler. (Defualt)
+        * .setHide('webhook') // Hide Webhook notifications (Defualt)
      */
     setHide(hidden) {
         if (hidden) {
@@ -88,19 +66,20 @@ class AntiCrash {
     /**
         * @example
         * new AntiCrash({ url: "DISCORD_WEBHOOK_URL" })
-        * .setShow() // Konsol ve Webhook bildirimlerini gösterir.
+        * .setShow() // Show Console and Webhook notifications
+        * 
+        * new AntiCrash()
+        * .setShow('console') // Show console notifications (Defualt)
         * 
         * new AntiCrash({ url: "DISCORD_WEBHOOK_URL" })
-        * .setShow('console') // Konsol bildirimlerini gösterir. (Defualt)
-        * 
-        * new AntiCrash({ url: "DISCORD_WEBHOOK_URL" })
-        * .setShow('webhook') // Webhook bildirimlerini gösterir.
+        * .setShow('webhook') // Show Webhook notifications
      */
     setShow(show) {
+        const ansi = require('ansi-colors');
+
         if (show) {
             if (show === 'webhook') {
                 (async () => {
-                    await this.checkWebhook(this.url);
                     if (!this.hook) {
                         const errorMessage = `${ansi.red(`You must enter the Discord Webhook URL!\n${ansi.white('Info:')} ${ansi.blueBright('For support: https://discord.gg/ktVdQYrtXF')}`)}`;
 
@@ -127,28 +106,34 @@ class AntiCrash {
     }
 
     /**
-     * Anti Crash sistemini başlatır.
+     * Starts the Anti Crash system
         * @example
-        * new AntiCrash({ url: "DISCORD_WEBHOOK_URL" }).start()
+        * new AntiCrash({ url: "DISCORD_WEBHOOK_URL" })
+        * .start()
+        * 
+        * new AntiCrash()
+        * .start() (Defualt)
      */
     start(options) {
+        const { EmbedBuilder, codeBlock } = require('discord.js');
+        const { info, error } = require('../Utils/consoleTools');
+        const ansi = require('ansi-colors');
+
         if (!this.hiddenConsole) {
-            sneaks.console.info('The AntiCrash system is running.');
+            info('The AntiCrash system is running.');
         };
 
-        process.on('uncaughtException', async (error) => {
+        process.on('uncaughtException', async (err) => {
             if (!this.hiddenConsole) {
-                sneaks.console.error(`${error.message}`);
+                error(`${err.message}`);
             };
 
             if (!this.hiddenWebhook) {
-                await this.checkWebhook(this.url);
-
                 const embed = new EmbedBuilder()
                     .setAuthor({ name: 'Unhandled Exception', iconURL: 'https://cdn.discordapp.com/emojis/1098632292378886224.png' })
-                    .setDescription(codeBlock('ansi', `[${ansi.red(error.name)}] : ${error.message}`))
+                    .setDescription(codeBlock('ansi', `[${ansi.red(err.name)}] : ${err.message}`))
                     .addFields(
-                        { name: '\u200B', value: codeBlock('js', error.stack), inline: false },
+                        { name: '\u200B', value: codeBlock('js', err.stack), inline: false },
                     )
                     .setColor('Red')
                     .setTimestamp()
@@ -157,32 +142,28 @@ class AntiCrash {
             };
         });
 
-        process.on('unhandledRejection', async (error) => {
+        process.on('unhandledRejection', async (err) => {
             if (!this.hiddenConsole) {
-                sneaks.console.error(`${error}`);
+                error(`${err}`);
             };
 
             if (!this.hiddenWebhook) {
-                await this.checkWebhook(this.url);
-
-                let name;
-                if (error instanceof TypeError) {
+                let name = 'Error';
+                if (err instanceof TypeError) {
                     name = 'TypeError';
-                } else if (error instanceof ReferenceError) {
+                } else if (err instanceof ReferenceError) {
                     name = 'ReferenceError';
-                } else if (error instanceof SyntaxError) {
+                } else if (err instanceof SyntaxError) {
                     name = 'SyntaxError';
-                } else if (error instanceof RangeError) {
+                } else if (err instanceof RangeError) {
                     name = 'RangeError';
-                } else if (error instanceof EvalError) {
+                } else if (err instanceof EvalError) {
                     name = 'EvalError';
-                } else {
-                    name = 'Error';
                 };
 
                 const embed = new EmbedBuilder()
                     .setAuthor({ name: 'Unhandled Rejection', iconURL: 'https://cdn.discordapp.com/emojis/1098632292378886224.png' })
-                    .setDescription(codeBlock('ansi', `[${ansi.red(name)}] : ${error}`))
+                    .setDescription(codeBlock('ansi', `[${ansi.red(name)}] : ${err}`))
                     .setColor('Red')
                     .setTimestamp()
 
@@ -190,19 +171,17 @@ class AntiCrash {
             };
         });
 
-        process.on('uncaughtExceptionMonitor', async (error) => {
+        process.on('uncaughtExceptionMonitor', async (err) => {
             if (!this.hiddenConsole) {
-                sneaks.console.error(`${error.message}`);
+                error(`${err.message}`);
             };
 
             if (!this.hiddenWebhook) {
-                await this.checkWebhook(this.url);
-
                 const embed = new EmbedBuilder()
                     .setAuthor({ name: 'Uncaught Exception Monitor', iconURL: 'https://cdn.discordapp.com/emojis/1098632292378886224.png' })
-                    .setDescription(codeBlock('ansi', `[${ansi.red(error.name)}] : ${error.message}`))
+                    .setDescription(codeBlock('ansi', `[${ansi.red(err.name)}] : ${err.message}`))
                     .addFields(
-                        { name: '\u200B', value: codeBlock('js', error.stack), inline: false },
+                        { name: '\u200B', value: codeBlock('js', err.stack), inline: false },
                     )
                     .setColor('Red')
                     .setTimestamp()
@@ -211,21 +190,19 @@ class AntiCrash {
             };
         });
 
-        process.on('warning', async (error) => {
+        process.on('warning', async (err) => {
             if (!this.hiddenConsole) {
-                sneaks.console.error(`${error.message}`);
+                error(`${err.message}`);
             };
 
             if (options.warn === true) return;
 
             if (!this.hiddenWebhook) {
-                await this.checkWebhook(this.url);
-
                 const embed = new EmbedBuilder()
                     .setAuthor({ name: 'Warning', iconURL: 'https://images.emojiterra.com/twitter/512px/26a0.png' })
-                    .setDescription(codeBlock('ansi', `[${ansi.yellow('Warn')}] : ${error.message}`))
+                    .setDescription(codeBlock('ansi', `[${ansi.yellow('Warn')}] : ${err.message}`))
                     .addFields(
-                        { name: '\u200B', value: error.stack, inline: false },
+                        { name: '\u200B', value: err.stack, inline: false },
                     )
                     .setColor('Yellow')
                     .setTimestamp()
