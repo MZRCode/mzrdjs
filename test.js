b6faf3a15aed7ee365bb5823095220dbde142e37
mzrdev <87870017+MZRCode@users.noreply.github.com>
2024-01-26 16:49:23 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,45 +1,4 @@
-const mzr = require('./file/index');
-//const kontr = require('./file/kelime-oyunu');
-//const { AntiCrash } = require('./file/class');
-
-// setTimeout(() => {
-//     const sonuc = mzr.calculate(10, 100);
-//     console.log('%' + sonuc + ' Percent ✔')
-// }, 500);
-
-// setTimeout(() => {
-//     const sonuc = mzr.random(10, 5);
-//     console.log(sonuc)
-// }, 1000);
-
-// setTimeout(() => {
-//     const sonuc = mzr.shortNumber(748412848123);
-//     console.log(sonuc + ' ShortNumber ✔')
-// }, 1500);
-
-// setTimeout(() => {
-//     const sonuc = mzr.mcmotd(`play.craftrise.tc`)
-//     console.log(sonuc + ' MC Motd ✔')
-// }, 2000);
-
-// setTimeout(() => {
-//     mzr.slashBuilder;
-// }, 2500);
-
-// /*setTimeout(() => {
-//     const sonuc = mzr.version;
-//     console.log(sonuc + ' Version ✔')
-// }, 3000);*/
-
-// setTimeout(() => {
-//     const sonuc = mzr.formatNumber(12381248125);
-//     console.log(sonuc)
-// }, 3000);
-
-// setTimeout(() => {
-//     const sonuc = mzr.timestamp(1695495014935)
-//     console.log(sonuc)
-// }, 3500)
+// const mzr = require('./file/index');;
 
 // setTimeout(() => {
 // const asama1 = 120000;
@@ -48,22 +7,10 @@ const mzr = require('./file/index');
 // console.log(asama3)
 // }, 100);
 
-// setTimeout(() => {
-//     kontr.tdk('otobüs').then((veri) => {
-//         console.log('Onay: ' + veri.onay);
-//         console.log('Not: ' + veri.not);
-//         console.log('Kelime: ' + veri.kelime);
-//         console.log('İlk Harf: ' + veri.ilkHarf);
-//         console.log('Son Harf: ' + veri.sonHarf);
-//         console.log('Lisân: ' + veri.lisan);
-//         console.log('Anlam: ' + veri.anlam);
-//     });
-// }, 4500);
-
-/*setTimeout(async () => {
-    const deneme = await mzr.leaderboard('users', 10, { dot: true });
-    console.log(deneme);
-}, 500);*/
+// setTimeout(async () => {
+//     const deneme = await mzr.leaderboard('users', 10, { dot: true });
+//     console.log(deneme);
+// }, 500);
 
 // console.log(mzr.generatePassword({ length: 16, allUppercase: true }));
 // console.log(mzr.generateCode({ length: 16, range: 4, symbol: '/', lowercase: true }));
