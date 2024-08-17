a9d57b0bfbfc848753062cd0ccae8086082cd684
mzrdev <87870017+MZRCode@users.noreply.github.com>
2024-08-17 23:35:18 +0300

    Delete file directory

--- file/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const { __exportStar } = require('tslib');
-
-__exportStar(require('./main'), exports);
-__exportStar(require('./AntiCrash'), exports);
-__exportStar(require('./kelime-oyunu'), exports);
